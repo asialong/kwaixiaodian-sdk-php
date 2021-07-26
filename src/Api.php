2ae3ce29f2ab3317780a3ea3fc7a557e3ad50a3a
william <75025268@qq.com>
2021-07-26 10:00:38 +0800

    update

--- src/Api.php
+++ src/Api.php
@@ -75,7 +75,7 @@ class Api extends AbstractAPI
         });
 
         if ('MD5' == $sign_method){
-            return strtolower(md5(sprintf('%s%s%s', $this->kwaixiaodian['oauth.access_token']->getSecret(), $paramsStr, $this->kwaixiaodian['oauth.access_token']->getSecret())));
+            return strtolower(md5(sprintf('%s%s', $paramsStr, $this->kwaixiaodian['oauth.access_token']->getSecret())));
         }
 
         return false;
